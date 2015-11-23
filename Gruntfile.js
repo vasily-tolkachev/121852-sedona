92e93882748d629b2453b6ca2f33b6a82ee029aa
Keks <keks@htmlacademy.ru>
2015-11-23 12:39:04 +0300

    :kissing_cat: Добавил сборку и ватч для SASS

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,37 @@ module.exports = function(grunt) {
   require("load-grunt-tasks")(grunt);
 
   var config = {
-    pkg: grunt.file.readJSON("package.json")
+    pkg: grunt.file.readJSON("package.json"),
+
+    sass: {
+      style: {
+        files: {
+          "css/style.css": "sass/style.scss"
+        }
+      }
+    },
+
+    postcss: {
+      options: {
+        processors: [
+          require("autoprefixer")({browsers: "last 2 versions"})
+        ]
+      },
+      style: {
+        src: "css/*.css"
+      }
+    },
+
+    watch: {
+      style: {
+        files: ["sass/**/*.scss"],
+        tasks: ["sass", "postcss"],
+        options: {
+          spawn: false,
+          livereload: true
+        }
+      }
+    }
   };
 
 
